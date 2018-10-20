d3f7f744aa3f8dae0665460c9d35a212a4d70152
Brett Cocking <brett@trademarktechnology.co.za>
2018-10-20 13:50:47 +0200

    Initial Commit

---
+++ iOS Practical Code Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Practical Code Test
+//
+//  Created by Brett Cocking on 2018/10/20.
+//  Copyright © 2018 Trademark Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
